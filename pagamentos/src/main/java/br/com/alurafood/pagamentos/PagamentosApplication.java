d618cb0970dab14cf005dfb997eb5846849b972d
Maria Ritha Nascimento <maria.ritha.2000@gmail.com>
2023-07-23 12:00:47 -0300

    Correção Bugs de comunicação e boas práticas

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
@@ -3,13 +3,9 @@ package br.com.alurafood.pagamentos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaServer
 @EnableEurekaClient
-@RestController
 public class PagamentosApplication {
 
 	public static void main(String[] args) {
--- service/src/main/java/br/com/alurafood/service/ServiceApplication.java
+++ service/src/main/java/br/com/alurafood/service/ServiceApplication.java
@@ -2,6 +2,7 @@ package br.com.alurafood.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
