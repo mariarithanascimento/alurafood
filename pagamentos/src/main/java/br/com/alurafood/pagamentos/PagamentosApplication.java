1b5210c45ead5853227be5b98ba3dc6da9947972
Maria Ritha Nascimento <maria.ritha.2000@gmail.com>
2023-07-22 21:46:10 -0300

    Correção falha de comunicação com Eureka

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
@@ -2,8 +2,12 @@ package br.com.alurafood.pagamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaServer
+@RestController
 public class PagamentosApplication {
 
 	public static void main(String[] args) {
