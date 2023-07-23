7817add440d8f3cb4eadd4d1d73d5d2e6c9291b7
Maria Ritha Nascimento <maria.ritha.2000@gmail.com>
2023-07-23 11:15:25 -0300

    Configurado application.properties e PagamentosApplication.java

--- service/src/main/java/br/com/alurafood/service/ServiceApplication.java
+++ service/src/main/java/br/com/alurafood/service/ServiceApplication.java
@@ -2,12 +2,14 @@ package br.com.alurafood.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceApplication.class, args);
+		SpringApplication.run(
+				ServiceApplication.class, args);
 	}
 
 }
